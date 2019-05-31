a77b775849667813c65e2e596338907ce4a944e2
GalGog <galynna00@gmail.com>
2019-05-31 20:50:57 +0100

    Rendering Authentication Status

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class GoogleAuth extends React.Component {
+    state ={ isSignedIn: null};
+
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId:
+                    '471441881731-vbjugerotlrlhm2o77eje2ajts1srjss.apps.googleusercontent.com',
+                scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({ isSignedIn: this.auth.isSignedIn.get()})
+
+            });
+        });
+    }
+
+    renderAuthButton(){
+        if (this.state.isSignedIn === null) {
+            return <div>I dont know are signed in</div>;
+        } else if (this.state.isSignedIn) {
+            return <div>I am signed in</div>;
+        } else {
+            return <div> I am not signed in</div>;
+        }
+    }
+
+    render() {
+     return <div>{this.renderAuthButton()}</div>
+ }
+}
+export default GoogleAuth;
